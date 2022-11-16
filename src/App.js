bf14f36fa59dba777c73004c0f3c4cc60108b2d4
Evannfries <evann.fries@yahoo.com>
2022-11-16 14:30:17 -0600

    register form submit console logs values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Home from './pages/home.js';
 import Header from './components/Navbar.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from "./pages/login.js";
+import Login from "./components/login.js";
 import Navbar from './components/Navbar.js';
-import Register from './pages/register.js';
+import Register from './components/register.js';
 
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
   <Router>
   <Navbar />
   <Routes>
+    <Route path='/posts/all' element={<Home />} />
     <Route path='/users/login' element={<Login />} />
     <Route path='/users/register' element={<Register />} />
   </Routes>
---
+++ src/components/register.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      img: ""
+    };
+    //use of bind() necessary to make 'this' work in the callback
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    const { username, password, img } = this.state;
+    console.log(username, password, img);
+  }
+
+  render(){
+  return (
+    <form className='register' onSubmit={this.handleSubmit}>
+        <h1>Register</h1>
+        <label>Username: </label>
+        <input 
+          type="text"
+          placeholder='username' 
+          onChange={(e) => this.setState({ username: e.target.value })}
+        />
+        <br />
+        <br />
+        <label>Password: </label>
+        <input 
+          type="text" 
+          placeholder='password'
+          onChange={(e) => this.setState({ password: e.target.value })}
+        />
+        <br />
+        <br />
+        <label>Profile Picture: </label>
+        <input
+          type="text"
+          placeholder="link to profile img"
+          onChange={(e) => this.setState({ img: e.target.value })}
+        />
+        <br />
+        <br />
+        <button type='submit'>Register</button>
+    </form>
+  )
+}
+}
+
+export default Register
