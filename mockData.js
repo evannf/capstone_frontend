cb3b236c11ae14e3144bc6fc73ef1d007f9c4d82
Evannfries <evann.fries@yahoo.com>
2022-11-26 17:09:43 -0600

    mock data added

---
+++ mockData.js
@@ -0,0 +1,37 @@
+const Users = [
+    {
+        id: 1,
+        username: "Beef Supreme",
+        profilePic: "assets/beef-supreme.png",
+        bio: "The King of Social Meatia"
+
+    },
+    {
+        id: 2,
+        username: "Danny Fillet",
+        profilePic: "assets/beef-supreme.png",
+        bio: "Best mate to Beef Supreme"
+    }
+]
+
+const Posts = [
+    {
+        id: 1,
+        userId: 1,
+        username: "Beef Supreme",
+        profilePic: "assets/beef-supreme",
+        body: "Weclome to the meat locker!",
+        likes: 12
+
+    },
+
+    {
+        id: 2,
+        userId: 2,
+        username: "Danny Fillet",
+        profilePic: "assets/beef-supreme",
+        body: "This place is the jam!",
+        likes: 12
+
+    }
+]
